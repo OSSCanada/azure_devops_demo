1182b242f2dc15152d159177260106824b53de7d
Raymond Kao <rakao@microsoft.com>
2019-02-05 22:57:32 -0500

    adding in +x to solve the release pipeline terraform exec permission problems

--- terraform/tf.init.sh
+++ terraform/tf.init.sh
@@ -8,6 +8,8 @@
 # export AZUREBLOBSTOREACCESSKEY=""
 # export TFSTATEFILENAME=""
 
+chmod +x terraform
+
 ./terraform init \
   -input=false \
   -backend-config="storage_account_name=$AZUREBLOBSTORAGEACCOUNTNAME" \
